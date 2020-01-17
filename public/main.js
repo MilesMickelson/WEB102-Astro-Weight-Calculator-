d210ea026e3df5a1c004d7899c8eb7ab449a6899
Miles Mickelson <milesmickelson87@gmail.com>
2020-01-17 11:51:51 -0800

    refactors project in addition to sass and express modules

---
+++ public/main.js
@@ -0,0 +1,37 @@
+var planets = [
+    ['Pluto', 0.06],
+    ['Neptune', 1.148],
+    ['Uranus', 0.917],
+    ['Saturn', 1.139],
+    ['Jupiter', 2.640],
+    ['Mars', 0.3895],
+    ['Moon', 0.1655],
+    ['Earth', 1],
+    ['Venus', 0.9032],
+    ['Mercury', 0.377],
+    ['Sun', 27.9]
+];
+
+var select = document.getElementById("planets");
+var userWeight = document.getElementById("user-weight");
+planets.forEach(function(element) {
+    var newSelect = document.createElement("option");
+    var planetNameOption = document.createTextNode(element[0]);
+    newSelect.appendChild(planetNameOption);
+    select.appendChild(newSelect);
+})
+
+function calculateWeight(userWeight, planetName) {
+    var planetName = document.getElementById("planets").selectedIndex;
+    var userWeight = document.getElementById("user-weight").value;
+    var gravity = planets[planetName][1];
+    var result = userWeight * gravity;
+    return result;
+}
+
+function handleClickEvent(userWeight, planetName) {
+    var userWeight = document.getElementById("user-weight").value;
+    var planetName = document.getElementById("planets").value;
+    var result = calculateWeight(userWeight, planetName);
+    document.getElementById("output").innerHTML = 'If you were on ' + planetName + ', you would weigh ' + result + 'lbs!';
+}
